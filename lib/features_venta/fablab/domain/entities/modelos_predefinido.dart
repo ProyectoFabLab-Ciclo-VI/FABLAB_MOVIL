c994be5d3ab184d9e38a25fb10c3c046276890f7
Alexis <alexis.delcastillo@upeu.edu.pe>
2024-10-17 18:00:04 -0500

    se agrego la model ModelosPredefinido

---
+++ lib/features_venta/fablab/domain/entities/modelos_predefinido.dart
@@ -0,0 +1,18 @@
+class ModelosPredefinido {
+  final int modelopredefinidoid;
+  final String nombre;
+  final String codigo;
+  final String url;
+  final String comentario;
+  final String imagen;
+  final double precio;
+
+  ModelosPredefinido(
+      {required this.modelopredefinidoid,
+      required this.nombre,
+      required this.codigo,
+      required this.url,
+      required this.comentario,
+      required this.imagen,
+      required this.precio});
+}
